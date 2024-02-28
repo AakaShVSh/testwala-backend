5cfe0a195ea2970b3c787feca5b1a61ce4dbe34f
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2024-02-28 20:24:51 +0530

    auth

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/User.model");
+const router = express.Router();
+
+const generateToken = (user) => {
+  return jwt.sign({ user }, "jakjsdgskasjbsabdjsd");
+};
+
+router.post("/registration", async (req, res) => {
+  try {
+    const { Email } = req.body;
+    const alreadyuser = await User.findOne({ Email });
+    console.log(alreadyuser);
+    if (!alreadyuser && Email != null) {
+      const user = await User.create(req.body);
+      const token = generateToken(req.body);
+      return res.send({ message: "registration success",token });
+    } else if (alreadyuser) {
+      return res.send({ message: "Email is already register" });
+    }
+  } catch (error) {
+    return res.send({ message: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { Email, Password } = req.body;
+    const alreadyuser = await User.findOne({ Email });
+    console.log(req.body);
+    //because we are finding in already user variable and below we are checking its password
+    const match = alreadyuser.checkPassword(Password);
+
+    if (alreadyuser && Email != null) {
+      if (!match) {
+        return res.send({ message: "Wrong Email or Password" });
+      } else if (match) {
+        let token = generateToken(req.body);
+        return res.send({ Message: "login success",data:req.body,token });
+      }
+    }
+  } catch (error) {
+    return res.send({Message:error.message});
+  }
+});
+
+router.patch("/change-password/:id",async (req,res) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.Password,10);
+    req.body.Password = hash;
+    const user = await User.findByIdAndUpdate(req.params.id,req.body,{
+      new:true,
+    }).lean().exec();
+    return res.send({message:"Password Updated Successfully",data:user})
+  } catch (error) {
+    return res.send({message:error.message});
+  }
+})
+module.exports = router;
---
+++ src/models/User.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const UserSchema = new mongoose.Schema(
+  {
+    Email: { type: String, trim: true, unique: true, match: /.+\@.+\..+/,required:true },
+    Password: { type: String, required: true },
+    // Tests: { type: [Object], required: true },
+    // Rank: { type: Number, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save",function (next){
+  const hash = bcrypt.hashSync(this.Password,10);
+  this.Password = hash;
+  next();
+})
+
+UserSchema.methods.checkPassword = function(password){
+  return bcrypt.compareSync(password,this.Password);
+}
+
+module.exports = mongoose.model("User",UserSchema);
