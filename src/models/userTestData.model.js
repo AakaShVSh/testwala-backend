8018f7d674327740d89c3a3ff75010ae275725e8
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2024-10-11 13:21:47 +0530

    upd

--- src/models/userTestData.model.js
+++ src/models/userTestData.model.js
@@ -4,7 +4,8 @@ const UserTestSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     section: { type: String, required: true },
-
+    subject:{type:String,required:true},
+    rank:{type:Number,required:true},
     difficultyLevel: {
       type: String,
       enum: ["easy", "medium", "hard"],
