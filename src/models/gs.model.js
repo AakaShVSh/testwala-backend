fe48f4bc3bc9bdbae373e5aca2ca5dc37770e004
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2024-11-18 17:49:21 +0530

    upd

--- src/models/gs.model.js
+++ src/models/gs.model.js
@@ -3,19 +3,23 @@ const mongoose = require("mongoose");
 const QuestionGsSchema = mongoose.Schema(
   {
     topic: { type: String, required: true },
-    type: { type: String, required: true },
+    section: { type: String, required: true },
+    subject: { type: String, required: true },
     difficultyLevel: {
       type: String,
       enum: ["easy", "medium", "hard"],
-      required: true,
+      // required: true,
       default: "easy",
     },
     question: [
       {
         qus: { type: String, required: true },
+        qush: { type: String, required: true },
         options: { type: Array, required: true },
+        optionsh: { type: Array, required: true },
         answer: { type: Number, required: true },
         explanation: { type: String, default: "" },
+        exam: { type: String, default: "" },
       },
     ],
   },
