574c104bc91d772fba1a4feda919d75dd2475698
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2026-03-01 13:34:26 +0530

    upd

--- src/src/controllers/auth.controller.js
+++ src/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User.model");
 const router = express.Router();
 const mailjet = require("node-mailjet").apiConnect(
