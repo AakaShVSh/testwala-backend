cb3ab1903977911a4da1b3abf704b4be554092d4
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2026-03-01 15:56:14 +0530

    upd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Port = process.env.Port || 80;
 const app = require("./src/App");
 const connect = require("./src/configs/db");
-
+ 
 app.listen(Port,async (req,res) => {
     try {
         await connect();
